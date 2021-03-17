5df9a16a6fc1a1174268c3023b92104103dc07f8
Christopher Williams <clwms16@gmail.com>
2021-03-17 01:38:43 +0000

    Added bow counts for the monsters.

--- main.py
+++ main.py
@@ -6,28 +6,37 @@ if charms == "1":
     ch = True
 
 hexCount = 0
+normCount = 0
+eliteCount = 0
+nameCount = 0
 print("Calculating...")
 for i in range (1000000): 
     if ch == False: #No Charm
         if random.randint(0,49) == 0: #Elite chance
             if random.randint(0,1499) == 0: #Named chance
                 hexCount += 1
+                nameCount += 1
             else: #Elite hex chance
                 if random.randint(0,1499) == 0:
                     hexCount += 1
+                    eliteCount += 1
         else: #Normal Seeker
             if random.randint(0,1499999) == 0:
                 hexCount += 1
+                normCount += 1
     else: # Seeker charm chance
         if random.randint(0,9) == 0: #Elite chance
             if random.randint(0,1499) == 0: #Named chance
                 hexCount += 1
+                nameCount += 1
             else: #Elite hex chance
                 if random.randint(0,1499) == 0:
                     hexCount += 1
+                    eliteCount += 1
         else: #Normal Seeker
             if random.randint(0,1499999) == 0:
                 hexCount += 1
+                normCount += 1
 
 output = "In 1,000,000 seeker kills, "
 if ch == True:
@@ -36,8 +45,9 @@ else:
     output += "without "
 output += "seeker charms, you got " + str(hexCount) + " bows"
 if hexCount != 0:
-    output += ", getting one on average every " + str((1000000/hexCount)) + "kills."
+    output += ", getting one on average every " + str((1000000/hexCount)) + " kills."
 else:
     output += "."
 
 print(output)
+print("\nYou got bows from the following creatures: \nNormal Soulgazers: " + str(normCount) + "\nElite Soulgazers: " + str(eliteCount) + "\nVeil-ripper Ozharakha: " + str(nameCount))
