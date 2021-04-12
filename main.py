23115f0276311a58302edf5719c669f59d08312f
RedX1000 <clwms16@gmail.com>
2021-04-12 03:14:26 -0500

    Worked on more graph code/math - Planning on moving the graphs to TKinter or trying to display both boxplot and displot side by side

--- main.py
+++ main.py
@@ -6,32 +6,40 @@ import hex
 
 print("Program started")
 charmList = []
-noCharmList = []
 percentage = 0
+rang = 1000
 
-for i in range(10):
+for i in range(rang):
     if (i % 10) == 0:
-        print(str(percentage) + "% of the way through.")
+        print(str(percentage) + "% of the way through true.")
         percentage += 1
     charmList.append(hex.hexRollsTrue())
-    noCharmList.append(hex.hexRollsFalse())
+percentage = 0
+for i in range(rang):
+    if (i % 10) == 0:
+        print(str(percentage) + "% of the way through false.")
+        percentage += 1
+    charmList.append(hex.hexRollsFalse())
 
 # hexCount = hex.hexRolls(ch)
 print("True: " + str(charmList))
-print("False: " + str(noCharmList))
 
-cl = pd.DataFrame(charmList, columns=['Hexhunter Bows', 'Average Bows', 'Normal Soulgazer', 'Elite Soulgazer',
-                                      'Veil-ripper Ozharakha'], dtype=float)
+cl = pd.DataFrame(charmList, columns=['Hexhunter Bows', 'Using charms', 'Average Bows'], dtype=float)
 print(cl)
 
-ncl = pd.DataFrame(noCharmList, columns=['Hexhunter Bows', 'Average Bows', 'Normal Soulgazer', 'Elite Soulgazer',
-                                         'Veil-ripper Ozharakha'], dtype=float)
-print(ncl)
-
-sns.boxplot(x='Average Bows',y='Hexhunter Bows',data=cl,)
-sns.boxplot(x='Average Bows',y='Hexhunter Bows',data=ncl,)
+#plot = sns.displot(data=cl, x='Hexhunter Bows', hue="Using charms", kde=True)
+plot = sns.displot(data=cl, x='Average Bows', hue="Using charms", kde=True)
+plot.set(ylabel="Count")
+#plot = sns.boxplot(y="Average Bows", data=cl, x="Using charms")
+#plot.set(xlabel="Using charms?")
+# sns.displot(ncl['Hexhunter Bows'], kind="kde")
+# sns.boxplot(x='Average Bows', y='Hexhunter Bows', data=cl, )
+# sns.boxplot(x='Average Bows', y='Hexhunter Bows', data=ncl, )
 plt.show()
 
+'''ncl = pd.DataFrame(noCharmList, columns=['Hexhunter Bows', 'Average Bows', 'Normal Soulgazer', 'Elite Soulgazer',
+                                         'Veil-ripper Ozharakha'], dtype=float)
+print(ncl)'''
 '''charms = input("Charms? Type 1 for yes or 0 for no: ")
 ch = False
 if charms == "1":
@@ -47,7 +55,6 @@ if hexCount != 0:
     output += ", getting one on average every " + str((1000000/hexCount)) + " kills."
 else:
     output += "."'''
-
 # print(output)
 # print("\nYou got bows from the following creatures: \nNormal Soulgazers: " + str(normCount) + "\nElite
 # Soulgazers: " + str(eliteCount) + "\nVeil-ripper Ozharakha: " + str(nameCount))
