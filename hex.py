5bc17d882c02253d513bf0229cb652c10ddcfd2a
Raphael Roberts <raphaelr@motorola.com>
2024-05-31 12:00:44 -0500

    Removed commented lines

--- hex.py
+++ hex.py
@@ -3,22 +3,16 @@ import random as rdm
 
 def hexRollsTrue():  # Seeker charm chance
     hexCount = 0
-    # normCount = 0
-    # eliteCount = 0
-    # nameCount = 0
     for i in range(100000):
         if rdm.randint(0, 9) == 0:  # Elite chance
             if rdm.randint(0, 1499) == 0:  # Named chance
                 hexCount += 1
-                # nameCount += 1
             else:  # Elite hex chance
                 if rdm.randint(0, 1499) == 0:
                     hexCount += 1
-                    # eliteCount += 1
         else:  # Normal Seeker
             if rdm.randint(0, 1499999) == 0:
                 hexCount += 1
-                # normCount += 1
     if hexCount != 0:
         bowsAverage = 100000 / hexCount
     else:
@@ -29,22 +23,16 @@ def hexRollsTrue():  # Seeker charm chance
 
 def hexRollsFalse():  # No charm
     hexCount = 0
-    # normCount = 0
-    # eliteCount = 0
-    # nameCount = 0
     for i in range(100000):
         if rdm.randint(0, 49) == 0:  # Elite chance
             if rdm.randint(0, 1499) == 0:  # Named chance
                 hexCount += 1
-                # nameCount += 1
             else:  # Elite hex chance
                 if rdm.randint(0, 1499) == 0:
                     hexCount += 1
-                    # eliteCount += 1
         else:  # Normal Seeker
             if rdm.randint(0, 1499999) == 0:
                 hexCount += 1
-                # normCount += 1
     if hexCount != 0:
         bowsAverage = 100000 / hexCount
     else:
--- main.py
+++ main.py
@@ -21,40 +21,11 @@ for i in range(rang):
         percentage += 1
     charmList.append(hex.hexRollsFalse())
 
-# hexCount = hex.hexRolls(ch)
 print("True: " + str(charmList))
 
 cl = pd.DataFrame(charmList, columns=['Hexhunter Bows', 'Using charms', 'Average Bows'], dtype=float)
 print(cl)
 
-#plot = sns.displot(data=cl, x='Hexhunter Bows', hue="Using charms", kde=True)
 plot = sns.displot(data=cl, x='Average Bows', hue="Using charms", kde=True)
 plot.set(ylabel="Count")
-#plot = sns.boxplot(y="Average Bows", data=cl, x="Using charms")
-#plot.set(xlabel="Using charms?")
-# sns.displot(ncl['Hexhunter Bows'], kind="kde")
-# sns.boxplot(x='Average Bows', y='Hexhunter Bows', data=cl, )
-# sns.boxplot(x='Average Bows', y='Hexhunter Bows', data=ncl, )
 plt.show()
-
-'''ncl = pd.DataFrame(noCharmList, columns=['Hexhunter Bows', 'Average Bows', 'Normal Soulgazer', 'Elite Soulgazer',
-                                         'Veil-ripper Ozharakha'], dtype=float)
-print(ncl)'''
-'''charms = input("Charms? Type 1 for yes or 0 for no: ")
-ch = False
-if charms == "1":
-    ch = True
-    
-output = "In 1,000,000 seeker kills, "
-if ch == True:
-    output += "with "
-else:
-    output += "without "
-output += "seeker charms, you got " + str(hexCount) + " bows"
-if hexCount != 0:
-    output += ", getting one on average every " + str((1000000/hexCount)) + " kills."
-else:
-    output += "."'''
-# print(output)
-# print("\nYou got bows from the following creatures: \nNormal Soulgazers: " + str(normCount) + "\nElite
-# Soulgazers: " + str(eliteCount) + "\nVeil-ripper Ozharakha: " + str(nameCount))
